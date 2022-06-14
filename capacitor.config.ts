9d8db3a0a5346e98bfa0950c95fc8f902c83e298
jowen-abucejo <jowennavalabucejo24@gmail.com>
2022-06-14 12:33:27 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kccf-desktop-client',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
